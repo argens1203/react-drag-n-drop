d40b9854fefdd36fcdb93176e6d0c2d0ef644d75
a/shopkyo <argens@connect.hku.hk>
2021-06-14 21:49:42 +0800

    use composite hook for dropzone

--- src/components/blocks/droppable-block.tsx
+++ src/components/blocks/droppable-block.tsx
@@ -1,37 +1,35 @@
 import React from "react";
 import {useDrop} from "react-dnd";
+import {useDispatch} from "react-redux";
 import {ItemTypes} from "../../drag/item-types.const";
 import {BlockTransfer} from "../../drag/block-transfer.type";
 import {setParent} from "../../redux/data.slice";
-import {useDispatch} from "react-redux";
 import {BlockDisplay, Hoverable} from "./block-display";
 
 export interface Block {
     id: string;
 }
 
-function withDropzone(){
-    return function (InnerComponent: React.ComponentType<Block & Hoverable>){
-        return function (props: Block){
-            const dispatch = useDispatch();
-            const [dropProps, drop] = useDrop(() => ({
-                accept: ItemTypes.BLOCK,
-                drop: (item: BlockTransfer) => {
-                    dispatch(setParent({child: item.id, parent: props.id}))
-                },
-                collect: (monitor) => ({
-                    hovered: monitor.isOver(),
-                    canDrop: monitor.canDrop(),
-                }),
-                canDrop: (item) => item.id !== props.id,
-            }));
-            return (
-                <div ref={drop}>
-                    <InnerComponent {...dropProps} {...props}/>
-                </div>
-            )
-        }
-    }
+function useDropzone(id: string) {
+    const dispatch = useDispatch();
+    return useDrop(() => ({
+        accept: ItemTypes.BLOCK,
+        drop: (item: BlockTransfer) => {
+            dispatch(setParent({child: item.id, parent: id}))
+        },
+        collect: (monitor) => ({
+            hovered: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+        canDrop: (item) => item.id !== id,
+    }));
 }
 
-export const DroppableBlock = withDropzone()(BlockDisplay);
+export function DroppableBlock(props: Block) {
+    const {id} = props;
+    const [dropProps, drop] = useDropzone(id);
+    const {hovered, canDrop} = dropProps || {};
+    return (
+        <BlockDisplay ref={drop} id={id} hovered={hovered} canDrop={canDrop}/>
+    )
+}
