5f3de69bf2715e2cd43d4dc861569ec53b455206
a/shopkyo <argens@connect.hku.hk>
2021-06-17 00:47:36 +0800

    wip drag to delete

--- src/blocks/block.tsx
+++ src/blocks/block.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 import {useSelector} from "react-redux";
 import {Box} from "@material-ui/core";
 import {RootState} from "../redux/store";
@@ -6,6 +6,8 @@ import {ReorderSpacing} from "./reorder-spacing";
 import {DroppableBlock} from "./droppable-block";
 import {BlockDragHandle} from "./block-drag-handle";
 import {BlockData} from './interfaces/block-data.interface';
+import {useDrag} from "react-dnd";
+import {ItemTypes} from "../drag/item-types.const";
 
 export function Block(props: BlockData) {
     const {id} = props;
@@ -14,14 +16,43 @@ export function Block(props: BlockData) {
     const childOrder = useSelector((state: RootState) => state.block.childrenOrder[id] || []);
     const children = childOrder.filter(id => childMap[id]);
 
+    const ref = createRef<HTMLDivElement>()
+    const [dragging, setDragging] = useState(false);
+    const[startX,setStartX] = useState<null | number>(null);
+    const[x, setX] = useState<null | number>(null);
+    const onMouseDown = (e: any) => {
+        setDragging(true);
+        setStartX(e.clientX);
+    };
+    const onMouseMove = (e: any) => {
+        if(dragging){
+            setX(e.clientX);
+        }
+    };
+    const onMouseUp = (e: any) =>{
+        setDragging(false);
+        setStartX(null);
+        setX(null);
+    }
+    let marginRight = 0;
+    if (x){
+        if (startX){
+            marginRight = startX - x;
+            console.log(marginRight);
+        }
+    }
     return (
         <>
             <Box flexDirection={'column'} display={'flex'} flex={1} alignItems={'stretch'}>
                 <ReorderSpacing id={id}/>
-                <Box flexDirection={'row'} display={'flex'} alignItems={'center'} position={'relative'} style={{border: `1px solid ${color}`}}>
-                    <BlockDragHandle id={id}/>
-                    <DroppableBlock id={id}/>
-                </Box>
+                <div ref={ref} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp}
+                     onMouseLeave={onMouseUp}
+                     style={{marginRight}}>
+                    <Box flexDirection={'row'} display={'flex'} alignItems={'center'} position={'relative'} style={{border: `1px solid ${color}`}}>
+                        <BlockDragHandle id={id}/>
+                        <DroppableBlock id={id}/>
+                    </Box>
+                </div>
             </Box>
             {children.map((id) => <Block key={id} id={id}/>)}
         </>
