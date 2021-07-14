9783c622974e320522f520d34fbca22d17a849c3
a/shopkyo <argens@connect.hku.hk>
2021-07-14 22:58:42 +0800

    mild refractor

---
+++ src/thunks/index.ts
@@ -0,0 +1,4 @@
+export * from './delete-block.thunk';
+export * from './get-block.thunk';
+export * from './init-block.thunk';
+export * from './set-parent.thunk';
--- src/thunks/set-parent.thunk.ts
+++ src/thunks/set-parent.thunk.ts
@@ -10,10 +10,11 @@ type Data = {
 export function setParent(data: Data) {
     return async function (dispatch: Dispatch) {
         const {child, parent} = data;
+        dispatch(setParentAction({child, parent}));
         // TODO: different relationship?
-        await createRelationship(child, parent)
-            .then((res) => {
-                dispatch(setParentAction({child, parent}));
-            })
+        // await createRelationship(child, parent)
+        //     .then((res) => {
+        //         dispatch(setParentAction({child, parent}));
+        //     })
     }
 }
