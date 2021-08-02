c162163d49db24d9dd4f04b764b887d4d427ee62
a/shopkyo <argens@connect.hku.hk>
2021-08-02 16:33:43 +0800

    working without api

--- src/thunks/delete-block.thunk.ts
+++ src/thunks/delete-block.thunk.ts
@@ -2,15 +2,17 @@ import {removeBlock as removeBlockAction} from '../redux/block/block.slice';
 import {AppThunkDispatch, AppThunkGetState} from "./thunk.type";
 import {IS_PARENT} from "../constants/relationship.const";
 import {removeRelationship} from "../redux/relationship/relationship.slice";
+import {setParent} from "./set-parent.thunk";
+import {getParent} from "../utils/get-parent.action";
 
 export function deleteBlock(id: string) {
     return async function (dispatch: AppThunkDispatch, getState: AppThunkGetState) {
         const state = getState();
-        const froms = Object.keys(state.relationship.reverseLookup[IS_PARENT][id] || {}).filter(k => !!k);
-        // TODO: handle childs
-        // const tos = Object.keys(state.relationship.lookup[IS_PARENT][id] || {}).filter(k => !!k);
-        froms.forEach(from => dispatch(removeRelationship({from, to: id, relationship: IS_PARENT})));
+        const parent = getParent(id, state);
+        const children = Object.entries(state.relationship.lookup[IS_PARENT][id] || {}).filter(([, v]) => !!v).map(([k,]) => k);
+        dispatch(removeRelationship({to: id, relationship: IS_PARENT}))
         dispatch(removeBlockAction(id));
+        children.forEach(child => dispatch(setParent({child, parent})));
         // TODO: APIs
     }
 }
