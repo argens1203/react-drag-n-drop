cee70bc64046eede884457256fd5f60d369e96cd
Argens <ng.argens@gmail.com>
2021-10-14 15:27:03 +0800

    feat(api): add api for getting links and data in one call

---
+++ src/middleware/combined/api/combined.api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import {BASE_URL} from "../../../config";
+import {BackendNode} from "../../nodes/entities";
+import {deserialize} from "class-transformer";
+import { BackendRelationship } from "../../relationships/entities";
+
+type CombinedReturn = {
+    links: BackendRelationship[],
+    nodes: BackendNode[],
+};
+
+export async function getChildren(id: string): Promise<CombinedReturn>{
+    const {nodes, links} = await axios.get(`${BASE_URL}/nodes/${id}/children`)
+    .then(res => res.data.data);
+    return {
+        nodes: nodes.map((n: any) => deserialize(BackendNode, JSON.stringify(n))),
+        links: links.map((l: any) => deserialize(BackendRelationship, JSON.stringify(l))),
+    }
+}
